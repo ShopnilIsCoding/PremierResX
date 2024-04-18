75a8071cd55dd63581e9053a64b9029ea800c3ad
ShopnilIsCoding <shopnilkhan766@gmail.com>
2024-04-18 17:56:11 +0600

    aos added in title

--- Pages/Home.jsx
+++ Pages/Home.jsx
@@ -52,8 +52,8 @@ const Home = () => {
     </div>
         </div>
         <div className="my-10 ">
-        <h1 className="text-center text-3xl font-extrabold mb-6">Discover Our Exclusive Estates</h1>
-        <p className="font-serif text-gray-600 text-center mx-auto mb-4 max-w-[90%] lg:max-w-xl">Explore our curated collection of luxurious estates around the world. From stunning penthouses with panoramic ocean views to grand mansions overlooking the sea, each property offers unparalleled luxury and breathtaking surroundings. Start your journey to finding your dream home today.</p>
+        <h1 className="text-center text-3xl font-extrabold mb-6" data-aos="zoom-out-right">Discover Our Exclusive Estates</h1>
+        <p className="font-serif text-gray-600 text-center mx-auto mb-4 max-w-[90%] lg:max-w-xl" data-aos="zoom-out-right">Explore our curated collection of luxurious estates around the world. From stunning penthouses with panoramic ocean views to grand mansions overlooking the sea, each property offers unparalleled luxury and breathtaking surroundings. Start your journey to finding your dream home today.</p>
             <Estates data={data}>
 
             </Estates>
